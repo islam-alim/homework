b9bbc951b8baf429023d3f8fab8f6b604d9ef833
islam <islamalimzhan0@gmail.com>
2025-01-24 22:13:32 +0500

    именно эту строчку изменил

--- Homework1.java
+++ Homework1.java
@@ -2,7 +2,7 @@ package kz.kaznu.islam.first_project;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Work done");
         float b;
         b = 10 / 3;
         System.out.println(b);
