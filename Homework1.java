2c94b49e0113be1634b9cec66987dab3432c7144
islam <islamalimzhan0@gmail.com>
2025-01-24 22:09:26 +0500

    new string added

--- Homework1.java
+++ Homework1.java
@@ -3,5 +3,10 @@ package kz.kaznu.islam.first_project;
 public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        float b;
+        b = 10 / 3;
+        System.out.println(b);
+        String str = "java";
+        System.out.println(str);
     }
 }
